600b81097b6cf46c0f74b2fcd35b66898c39db17
haroldstyven <harithodevoz@gmail.com>
2026-01-29 01:16:00 -0500

    inteegracion de supabase cloud y dependencias de docker eliminadas

---
+++ lib/core/constants.dart
@@ -0,0 +1,4 @@
+const String supabaseUrl = 'https://dgdmhsulxonxbygoufcf.supabase.co';
+const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRnZG1oc3VseG9ueGJ5Z291ZmNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk2NTYzMzMsImV4cCI6MjA4NTIzMjMzM30.HJp61QNU_AsEBTNufaIvVhqc-MDFNbcdqPjrLfYu5F4';
+
+// TODO: Reemplazar con las credenciales de Supabase Cloud
