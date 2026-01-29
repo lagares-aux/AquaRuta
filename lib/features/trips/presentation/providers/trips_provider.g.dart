89aa42020cd4bf1a5608b838603f50cc82e574e7
Harold Styven <hlagares@utb.edu.co>
2026-01-29 09:38:58 +0000

    nuevo provider 2.0

--- lib/features/trips/presentation/providers/trips_provider.g.dart
+++ lib/features/trips/presentation/providers/trips_provider.g.dart
@@ -39,5 +39,21 @@ final tripsStreamProvider = AutoDisposeStreamProvider<List<TripModel>>.internal(
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
 typedef TripsStreamRef = AutoDisposeStreamProviderRef<List<TripModel>>;
+String _$bookingControllerHash() => r'8780721b81cd69211e0cd79c665fdd08862370b1';
+
+/// See also [BookingController].
+@ProviderFor(BookingController)
+final bookingControllerProvider =
+    AutoDisposeAsyncNotifierProvider<BookingController, void>.internal(
+  BookingController.new,
+  name: r'bookingControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$bookingControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$BookingController = AutoDisposeAsyncNotifier<void>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
