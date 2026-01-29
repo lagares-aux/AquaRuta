7070a988335b6824b114b0c62a2cf8f31767ce3e
Harold Styven <hlagares@utb.edu.co>
2026-01-29 18:21:56 +0000

    builder actualizado

---
+++ lib/features/auth/presentation/providers/user_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userProfileHash() => r'dc91d46ba9213976ccc14bf608fe574dc97dbc2e';
+
+/// See also [userProfile].
+@ProviderFor(userProfile)
+final userProfileProvider = AutoDisposeFutureProvider<UserProfile>.internal(
+  userProfile,
+  name: r'userProfileProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$userProfileHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef UserProfileRef = AutoDisposeFutureProviderRef<UserProfile>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
