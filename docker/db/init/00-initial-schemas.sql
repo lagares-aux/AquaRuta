4b3062b0a0b192e1a9771d99297dc35c076f311c
haroldstyven <harithodevoz@gmail.com>
2026-01-29 00:54:45 -0500

    usuario supabase_storage_admin

--- docker/db/init/00-initial-schemas.sql
+++ docker/db/init/00-initial-schemas.sql
@@ -1,9 +1,23 @@
+-- Roles de sistema
+CREATE ROLE anon NOLOGIN;
+CREATE ROLE authenticated NOLOGIN;
+CREATE ROLE service_role NOLOGIN;
+CREATE ROLE supabase_admin WITH LOGIN PASSWORD 'postgres' SUPERUSER;
+CREATE ROLE supabase_auth_admin WITH LOGIN PASSWORD 'postgres' NOINHERIT CREATEDB CREATEROLE;
+CREATE ROLE supabase_storage_admin WITH LOGIN PASSWORD 'postgres' NOINHERIT CREATEDB CREATEROLE;
+CREATE ROLE supabase_replication_admin WITH LOGIN PASSWORD 'postgres' NOINHERIT CREATEDB CREATEROLE;
+
+-- Permisos básicos
+GRANT ALL PRIVILEGES ON DATABASE postgres TO supabase_admin;
+GRANT ALL PRIVILEGES ON DATABASE postgres TO supabase_auth_admin;
+GRANT ALL PRIVILEGES ON DATABASE postgres TO supabase_storage_admin;
+
 CREATE SCHEMA IF NOT EXISTS auth;
 CREATE SCHEMA IF NOT EXISTS storage;
 CREATE SCHEMA IF NOT EXISTS realtime;
 CREATE SCHEMA IF NOT EXISTS _realtime;
 
--- Roles necesarios
+-- Roles necesarios adicionales (idempotentes)
 CREATE ROLE IF NOT EXISTS anon NOLOGIN;
 CREATE ROLE IF NOT EXISTS authenticated NOLOGIN;
 CREATE ROLE IF NOT EXISTS service_role NOLOGIN;
@@ -17,6 +31,6 @@ BEGIN
   END IF;
 END$$;
 
--- Grants básicos
+-- Grants básicos adicionales
 GRANT USAGE ON SCHEMA public TO postgres, anon, authenticated, service_role;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO postgres, anon, authenticated, service_role;
